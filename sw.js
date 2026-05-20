d269f9b765dd97cccfbd23c01f2b3951ce929d89
0112358 <gx0112358@gmail.com>
2026-05-19 18:08:50 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("app-cache").then((cache) => {
+      return cache.addAll([
+        "/swIDE/",          // index
+        "/swIDE/icono.jpg", // ícono
+        "/swIDE/manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
