a689739afd6499231071295d4eadbfc04295e234
0112358 <gx0112358@gmail.com>
2026-06-14 12:28:26 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // 🔧 Configuración: cambia solo estas dos constantes
 const PROJECT_NAME = "swIDE"; // nombre del proyecto
-const VERSION = "v9";                 // versión actual
+const VERSION = "v10";                 // versión actual
 
 const CACHE_NAME = `${PROJECT_NAME}-cache-${VERSION}`;
 const FILES_TO_CACHE = [
